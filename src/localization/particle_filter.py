2771eaeddbe9566b1771ea66691a96c960db7150
Thomas Fisher <tfisher9@mit.edu>
2023-03-24 16:05:14 -0400

    Implemented particle matrix average

--- src/localization/particle_filter.py
+++ src/localization/particle_filter.py
@@ -4,6 +4,7 @@ import numpy as np
 import rospy
 from sensor_model import SensorModel
 from motion_model import MotionModel
+from collections import defaultdict
 
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
@@ -31,10 +32,10 @@ class ParticleFilter:
         odom_topic = rospy.get_param("~odom_topic", "/odom")
 
         self.laser_sub = rospy.Subscriber(scan_topic, LaserScan,
-                                          self.laser_callback(), # TODO: Fill this in
+                                          self.laser_callback, # TODO: Fill this in
                                           queue_size=1)
         self.odom_sub  = rospy.Subscriber(odom_topic, Odometry,
-                                          self.odom_callback(), # TODO: Fill this in
+                                          self.odom_callback, # TODO: Fill this in
                                           queue_size=1)
 
         #  *Important Note #2:* You must respond to pose
@@ -43,7 +44,7 @@ class ParticleFilter:
         #     "Pose Estimate" feature in RViz, which publishes to
         #     /initialpose.
         self.pose_sub  = rospy.Subscriber("/initialpose", PoseWithCovarianceStamped,
-                                          self.generate_particles(), # TODO: Fill this in
+                                          self.generate_particles,
                                           queue_size=1)
 
         #  *Important Note #3:* You must publish your pose estimate to
@@ -58,8 +59,12 @@ class ParticleFilter:
         self.motion_model = MotionModel()
         self.sensor_model = SensorModel()
 
+        self.rate = 20
+
+        rospy.Rate(self.rate)
+
         # Initialize the particles
-        self.particles = self.generate_particles()
+        # self.particles = self.generate_particles()
 
         # Implement the MCL algorithm
         # using the sensor model and the motion model
@@ -71,7 +76,7 @@ class ParticleFilter:
         # Publish a transformation frame between the map
         # and the particle_filter_frame.
 
-    def generate_particles(self):
+    def generate_particles(self, pose_msg):
         """
         Generates a matrix of particles for the filter at initialization
 
@@ -82,7 +87,8 @@ class ParticleFilter:
                 [x1 y0 theta1]
                 [    ...     ]
         """
-        return np.zeros(3, self.num_particles)
+        self.particles = np.zeros((3, self.num_particles))
+        print(self.compute_particle_avg())
 
     def laser_callback(self, laser_msg):
         """
@@ -102,7 +108,67 @@ class ParticleFilter:
         args:
             odom_msg: A Odometry msg
         """
-        self.motion_model.evaluate(self.particles, )
+
+        # create the [dx, dy, dtheta] from the odom msg
+        odom_list = self.rate * np.array([odom_msg.twist.linear.x, odom_msg.twist.linear.y, odom_msg.twist.angular.z])
+
+        self.motion_model.evaluate(self.particles, odom_list)
+
+    def compute_particle_avg(self):
+        """
+        Returns an [x, y, theta] from taking the average of self.particles
+        """
+
+        discretization_factor = 100
+
+        x_y_indices = np.array([self.particles[0] // discretization_factor, 
+                               self.particles[1] // discretization_factor])
+
+        coord_freq = defaultdict(int)
+        for i in range(x_y_indices.shape[1]):
+            coord_freq[(x_y_indices[0][i], x_y_indices[1][i])] += 1
+    
+
+        most_freq_key = max(coord_freq, key=coord_freq.get)
+
+        avg_x = 0
+        avg_y = 0
+        avg_theta = 0
+        count = 0
+        for i in range(self.particles.shape[1]):
+            if (self.particles[0][i] // discretization_factor == most_freq_key[0] and 
+                self.particles[1][i] // discretization_factor  == most_freq_key[1]):
+                avg_x += self.particles[0][i]
+                avg_y += self.particles[1][i]
+                avg_theta += self.particles[2][i]
+                count += 1
+
+        return [avg_x / count, avg_theta / count, avg_theta / count]
+
+
+        # grid_map = defaultdict(int)
+
+        # # enter all the x/y pairs into the grid
+        # for particle_vect in self.particles:
+        #     bucket_key = (particle_vect[0] // discretization_factor, particle_vect[1] // discretization_factor, particle_vect[2])
+        #     grid_map[bucket_key] += 1
+
+        # # find the bucket key with the highest number of entries
+        # highest_bucket_key = max(grid_map, key=grid_map.get)
+
+        # # take the average of all x/y pairs within the most densely populated bucket
+        # coords_to_avg = [np.empty([2, 3])]
+        # for bucket_key in grid_map.keys():
+        #     if bucket_key[0] >= highest_bucket_key[0] and bucket_key[0] <= highest_bucket_key[0] + discretization_factor:
+        #         if bucket_key[1] >= highest_bucket_key[1] and bucket_key[1] <= highest_bucket_key[1] + discretization_factor:
+        #             coords_to_avg = np.vstack([coords_to_avg, [bucket_key[0], bucket_key[1], bucket_key[2]]])
+
+        # return [np.mean(coords_to_avg[:0]), np.mean(coords_to_avg[:1]), np.mean(coords_to_avg[:2])]
+    
+
+        return None
+
+
     
     
 if __name__ == "__main__":
